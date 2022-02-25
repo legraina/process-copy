14b21214905b90167aa38eb4a8553e252232596c
legraina <antoine.legrain@gmail.com>
2022-02-25 09:37:58 -0500

    Modifying latex front page to put the bareme outside of the box.

--- python/process_copy/config.py
+++ python/process_copy/config.py
@@ -4,7 +4,7 @@ re_mat = '[1-2]\\d{6}(?=(?:\\D|$))'
 grade_box = {
     "devoir": {
         'grade': (.1, .9, .6, .9),  # x1, x2, y1, y2 in % of the page width and height
-        'trim': [(-1, 3)]  # i, n: n number of digits to remove at the end of the ith box
+        # 'trim': [(-1, 3)]  # i, n: n number of digits to remove at the end of the ith box
     },
     "exam": {
         'grade': (0.86, 0.94, 0.22, 0.5)
